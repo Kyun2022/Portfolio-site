447ac0185d58c672079cd0e7806c46f8122ca65d
kyun2022 <fm13071303@rx.tnc.ne.jp>
2025-05-02 19:14:41 +0900

    Initial commit

---
+++ src/components/ContactForm.tsx
@@ -0,0 +1,51 @@
+import React, { useRef, useState } from 'react';
+import toast, { Toaster } from 'react-hot-toast';
+
+const ContactForm = () => {
+  const [isSubmitted, setIsSubmitted] = useState(false);
+  const formRef = useRef<HTMLFormElement>(null);
+  const handleSubmit: React.FormEventHandler<HTMLFormElement> = async (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+    if (!formData.get('email')) {
+      toast.error('Please enter your email');
+      return;
+    }
+    // hit endpoint
+    await new Promise((resolve) => setTimeout(resolve, 1500));
+    toast.success("Thank! I'll be in touch soon.", {
+      duration: 4000,
+    });
+    formRef.current?.reset();
+  };
+
+  return (
+    <>
+      <form className="contact-form" ref={formRef} onSubmit={handleSubmit}>
+        <h2>Let's Connect</h2>
+        <p>
+          お問い合わせ、見積もり、
+          <br />
+          またはコラボレーションについては
+          <br />
+          下記までご連絡ください
+        </p>
+        <label>
+          your email
+          <input
+            type="email"
+            name="email"
+            autoComplete="email"
+            placeholder="e.g., kyundesign2022@gmail.com"
+          />
+        </label>
+        <button className="link" type="submit" disabled={isSubmitted}>
+          {isSubmitted ? 'Submitting...' : 'Contact Me'}
+        </button>
+      </form>
+      <Toaster />
+    </>
+  );
+};
+
+export default ContactForm;
---
+++ src/data/siteData.ts
@@ -0,0 +1,5 @@
+export const baseData = {
+  title: 'Masanao Fukuishi Portfolio Site',
+  description:
+    "Welcome to my portfolio site! I'm a passionate front-end engineer crafting responsive and intuitive user interfaces with clean code and attention to detail.",
+};
